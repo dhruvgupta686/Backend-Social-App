62575a1727e265c01fc755766fad56cafcf51726
dhruvgupta686 <147935223+dhruvgupta686@users.noreply.github.com>
2025-08-01 12:23:47 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb+srv://harshpruthi:SoxzxoS4jLZxz89M@namastenode.gwoj6.mongodb.net/");
-
-//all we need to connect to our cluster 
-
-//not a good away, wrap this in async function is better, mongoose.connect gives us a promise
-
 const connectDB = async()=>{
-    // await mongoose.connect("mongodb+srv://harshpruthi:SoxzxoS4jLZxz89M@namastenode.gwoj6.mongodb.net/");
-    // to refer to a specific database /devTinder
 
-    await mongoose.connect("mongodb+srv://harshpruthi:9yyDXUOdUdHFzBbz@namastenode.gwoj6.mongodb.net/devTinder");
+
+    await mongoose.connect(MONGO_URL);
 }
 
 module.exports = connectDB;
